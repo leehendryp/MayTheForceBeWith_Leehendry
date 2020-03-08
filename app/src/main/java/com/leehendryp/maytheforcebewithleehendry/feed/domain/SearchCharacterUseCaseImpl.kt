00f5700f79ab2fd80c78361def34b83811df11c1
leehendryp <leehendryp@gmail.com>
2020-03-07 23:22:50 -0300

    feat(use-cases): implement use case calls to repository

---
+++ app/src/main/java/com/leehendryp/maytheforcebewithleehendry/feed/domain/SearchCharacterUseCaseImpl.kt
@@ -0,0 +1,8 @@
+package com.leehendryp.maytheforcebewithleehendry.feed.domain
+
+import javax.inject.Inject
+
+class SearchCharacterUseCaseImpl
+@Inject constructor(private val peopleRepository: PeopleRepository) : SearchCharacterUseCase {
+    override suspend fun execute(query: String): People = peopleRepository.searchCharacterBy(query)
+}
---
+++ app/src/test/java/com/leehendryp/maytheforcebewithleehendry/feed/domain/SearchCharacterUseCaseImplTest.kt
@@ -0,0 +1,36 @@
+package com.leehendryp.maytheforcebewithleehendry.feed.domain
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.leehendryp.maytheforcebewithleehendry.core.MainCoroutineRule
+import io.mockk.coVerify
+import io.mockk.spyk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SearchCharacterUseCaseImplTest {
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var repo: PeopleRepository
+    private lateinit var useCase: SearchCharacterUseCase
+
+    @Before
+    fun `set up`() {
+        repo = spyk()
+        useCase = SearchCharacterUseCaseImpl(repo)
+    }
+
+    @Test
+    fun `should call repository with the correct query upon call`() = runBlocking {
+        useCase.execute("Rey")
+
+        coVerify(exactly = 0) { repo.searchCharacterBy("Leia") }
+        coVerify(exactly = 1) { repo.searchCharacterBy("Rey") }
+    }
+}
