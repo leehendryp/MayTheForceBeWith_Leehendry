2a55ebfb5016640a8c6c03cfaf155930449124e9
leehendryp <leehendryp@gmail.com>
2020-03-07 22:14:27 -0300

    feat(api): add interface for network data fetch from api

---
+++ app/src/main/java/com/leehendryp/maytheforcebewithleehendry/core/StarWarsApi.kt
@@ -0,0 +1,25 @@
+package com.leehendryp.maytheforcebewithleehendry.core
+
+import com.leehendryp.maytheforcebewithleehendry.feed.data.entities.CharacterResponse
+import com.leehendryp.maytheforcebewithleehendry.feed.data.entities.PeopleResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val BASE_URL = "https://swapi.co/api/"
+
+interface StarWarsApi {
+    @GET("people/")
+    suspend fun fetchPeople(
+        @Query("page") offset: Int
+    ): PeopleResponse
+
+    @GET("people/")
+    suspend fun fetchCharacterBy(
+        @Query("id") id: Int
+    ): CharacterResponse
+
+    @GET("people/")
+    suspend fun searchCharacterBy(
+        @Query("search") name: String
+    ): PeopleResponse
+}
