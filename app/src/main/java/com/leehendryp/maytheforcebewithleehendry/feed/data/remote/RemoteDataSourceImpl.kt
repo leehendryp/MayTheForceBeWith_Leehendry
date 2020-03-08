843525737fa0450c08e14ea095c40f27b938f8a0
leehendryp <leehendryp@gmail.com>
2020-03-07 23:03:51 -0300

    feat(remote-data-source): implement class to use data from api
    Add test to drive the proper development of the remote data source
    implementation, based on the abstractions set out in its interface;
    Implement the details of the class based on the tests applied.

---
+++ app/src/main/java/com/leehendryp/maytheforcebewithleehendry/feed/data/remote/RemoteDataSourceImpl.kt
@@ -0,0 +1,33 @@
+package com.leehendryp.maytheforcebewithleehendry.feed.data.remote
+
+import com.leehendryp.maytheforcebewithleehendry.core.StarWarsApi
+import com.leehendryp.maytheforcebewithleehendry.core.utils.coTryCatch
+import com.leehendryp.maytheforcebewithleehendry.feed.data.CouldNotFetchCharacterError
+import com.leehendryp.maytheforcebewithleehendry.feed.data.CouldNotFetchPeopleError
+import com.leehendryp.maytheforcebewithleehendry.feed.data.CouldNotSearchCharacterError
+import com.leehendryp.maytheforcebewithleehendry.feed.data.toCharacter
+import com.leehendryp.maytheforcebewithleehendry.feed.data.toPeople
+import com.leehendryp.maytheforcebewithleehendry.feed.domain.Character
+import com.leehendryp.maytheforcebewithleehendry.feed.domain.People
+import javax.inject.Inject
+
+class RemoteDataSourceImpl @Inject constructor(private val api: StarWarsApi) : RemoteDataSource {
+    companion object {
+        private const val REMOTE_SOURCE_ERROR = "Failed to fetch data from remote source"
+    }
+
+    override suspend fun fetchPeople(page: Int): People = coTryCatch(
+        { api.fetchPeople(page).toPeople() },
+        { CouldNotFetchPeopleError(REMOTE_SOURCE_ERROR, it) }
+    )
+
+    override suspend fun fetchCharacterBy(id: Int): Character = coTryCatch(
+        { api.fetchCharacterBy(id).toCharacter() },
+        { CouldNotFetchCharacterError(REMOTE_SOURCE_ERROR, it) }
+    )
+
+    override suspend fun searchCharacterBy(name: String): People = coTryCatch(
+        { api.searchCharacterBy(name).toPeople() },
+        { CouldNotSearchCharacterError(REMOTE_SOURCE_ERROR, it) }
+    )
+}
