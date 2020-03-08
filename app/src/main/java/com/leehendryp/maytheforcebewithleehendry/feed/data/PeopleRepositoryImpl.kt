e9ef3a9e5470afc090ea70437315dcf7967e8e2d
leehendryp <leehendryp@gmail.com>
2020-03-07 23:06:23 -0300

    feat(repo): implement class to decide how and where to find data
    Add tests to drive the proper development of the repository, in
    order to provide a clean API for use cases to fetch data from,
    without having to worry with the details of where it is available;
    Implement the repository class based on the test, choosing where
    to fetch data from second to network availability and API response.

---
+++ app/src/main/java/com/leehendryp/maytheforcebewithleehendry/feed/data/PeopleRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.leehendryp.maytheforcebewithleehendry.feed.data
+
+import com.leehendryp.maytheforcebewithleehendry.core.utils.NetworkUtils
+import com.leehendryp.maytheforcebewithleehendry.feed.data.remote.RemoteDataSource
+import com.leehendryp.maytheforcebewithleehendry.feed.domain.Character
+import com.leehendryp.maytheforcebewithleehendry.feed.domain.People
+import com.leehendryp.maytheforcebewithleehendry.feed.domain.PeopleRepository
+import javax.inject.Inject
+
+class PeopleRepositoryImpl @Inject constructor(
+    private val networkUtils: NetworkUtils,
+    private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource
+) : PeopleRepository {
+    override suspend fun fetchPeople(page: Int): People {
+        return if (networkUtils.isInternetAvailable()) {
+            try {
+                remoteDataSource.fetchPeople(page)
+                    .also { save(it) }
+            } catch (error: Throwable) {
+                localDataSource.fetchPeople()
+            }
+        } else {
+            localDataSource.fetchPeople()
+        }
+    }
+
+    override suspend fun fetchCharacterBy(id: Int): Character {
+        return if (networkUtils.isInternetAvailable()) remoteDataSource.fetchCharacterBy(id)
+        else localDataSource.fetchCharacterBy(id)
+    }
+
+    override suspend fun searchCharacterBy(name: String): People {
+        return if (networkUtils.isInternetAvailable()) remoteDataSource.searchCharacterBy(name)
+        else localDataSource.searchCharacterBy(name)
+    }
+
+    override suspend fun save(people: People) = localDataSource.save(people)
+}
